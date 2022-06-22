6550bf3b9f0f4d9b2c0ea37f5f4b5fcd550c801b
Grzegorz Węgorzewski <bisect@me.com>
2022-06-22 03:40:35 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+
+function App() {
+ 
+  return (
+    <div className="nav-area">
+       <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/data.js
@@ -0,0 +1,70 @@
+export const menuItems = [
+    {
+      title: "Home",
+    },
+    {
+      title: "Services",
+      submenu: [
+        {
+          title: "Web design",
+        },
+        {
+          title: "Web development",
+        },
+        {
+          title: "Frontend",
+        },
+        {
+          title: "Backend",
+        },
+        {
+          title: "SEO",
+        },
+      ],
+    },
+    {
+      title: "Software",
+      submenu: [
+        {
+          title: "One Drive",
+        },
+        {
+          title: "Outlook",
+        },
+        {
+          title: "Skype",
+        },
+        {
+          title: "Teams",
+        },
+        {
+          title: "One Note",
+        },
+      ],
+    },
+    {
+      title: "Entertainment",
+      submenu: [
+        {
+          title: "Azure",
+        },
+        {
+          title: "Xbox Live",
+        },
+        {
+          title: "Dynamics 365",
+        },
+      ],
+    },
+    {
+      title: "About",
+      submenu: [
+        {
+          title: "Who we are",
+        },
+        {
+          title: "Our values",
+        },
+      ],
+    },
+  ];
